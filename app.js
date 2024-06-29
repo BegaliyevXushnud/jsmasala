2b49301d684e7db5c5c2c299b4c86100d184b4b0
BegaliyevXushnud <xushnudpubgmn1@gmail.com>
2024-06-29 22:54:21 +0500

    added about

--- app.js
+++ app.js
@@ -13,6 +13,7 @@
 //     console.log(`bo'linmasi :${parseInt(number1 / number2)} `);
 // }
 
+
 // 1-Masala
 
 
@@ -21,13 +22,83 @@
 
 
 
-let n1 = +prompt();
-if(isNaN(n1)){
-         console.log("Son kiriting");
-     }else{
-        if(n1 > 0){
-            console.log(n1 - 1);
-        }else{
-            console.log(n1 + 1);
-        }
-     }
+// let n1 = +prompt();
+// if(isNaN(n1)){
+//          console.log("Son kiriting");
+//      }else{
+//         if(n1 > 0){
+//             console.log(n1 - 1);
+//         }else{
+//             console.log(n1 + 1);
+//         }
+//      }
+
+
+//      2-masala
+
+
+
+
+
+
+
+
+// const reverse = (num) => {
+//     if (num >= 10 && num <= 99) {
+//         const tens = Math.floor(num / 10);
+//         const units = num % 10;
+//         const reversedNum = units * 10 + tens;
+//         return reversedNum;
+//     } else {
+//         return "Iltimos, faqat ikki xonali son kiriting.";
+//     }
+// };
+
+// console.log(reverse(42)); 
+// console.log(reverse(89)); 
+// console.log(reverse(73));  
+// console.log(reverse(23)); 
+
+
+// 3-masala
+
+
+
+// const reverseThree = (num) => {
+//     if (num >= 100 && num <= 999) {
+//         const hundreds = Math.floor(num / 100);
+//         const tens = Math.floor((num % 100) / 10);
+//         const units = num % 10;
+//         const reverseNumber = units * 100 + tens * 10 + hundreds;
+//         return reverseNumber;
+//     } else {
+//         return "Iltimos, faqat uch xonali son kiriting.";
+//     }
+// };
+
+// console.log(reverseThree(123)); 
+// console.log(reverseThree(112));
+// console.log(reverseThree(789)); 
+// console.log(reverseThree(422));  
+// console.log(reverseThree(124)); 
+
+
+// 4-masala
+
+
+
+// function ball(mavqie) {
+//     if(mavqie >= 70 && mavqie <= 100){
+//         return "budjet";
+//     }else if(mavqie >= 50 && mavqie <= 70) {
+//         return "contract";
+//     }else if(mavqie <= 50){
+//         return "yiqildi";
+//     }else{
+//         return "bunday ball yuq"
+//     }
+// }
+// console.log(ball(80));
+
+
+// 5-masala
